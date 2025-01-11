6bf00d1ec382df962ea9d9785b1245a100530120
Youngjae <2015fromjeki@gmail.com>
2025-01-11 17:17:08 +1100

    Product Model has been created

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,4 @@
+export const getAllProducts = async (req, res) => {
+  try {
+  } catch (error) {}
+};
--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,11 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 // Setting
+import { connectDB } from "./lib/connectDB.js";
 dotenv.config({ path: "/Users/youngjaekim/Desktop/e-commerce/backend/.env" });
 // Routes
 import authRoutes from "./routes/auth.route.js";
-import { connectDB } from "./lib/connectDB.js";
+import productRoutes from "./routes/product.route.js";
 
 const PORT = process.env.PORT || 5009;
 
@@ -14,10 +15,21 @@ const app = express();
 // SET THE MIDDLEWARE
 app.use(express.json());
 app.use(cookieParser());
+// Serve static files or basic pages
+app.get("/about", (req, res) => {
+  res.send("<h1>About Us</h1>");
+});
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contact Us</h1>");
+});
 
 // Routess
-app.use("/auth", authRoutes);
+// API means user needs an authentication
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
+// Listen PORT
 app.listen(PORT, () => {
   console.log(`SERVER IS RUNNING IN ${PORT} `);
   connectDB();
